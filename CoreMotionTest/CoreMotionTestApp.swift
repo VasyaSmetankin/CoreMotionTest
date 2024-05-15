583ec92bd0749cf2e653e8f603fe0b491dc02460
VasyaSmetankin <medvedevkon70@gmail.com>
2024-05-15 17:25:20 +0300

    Initial Commit

---
+++ CoreMotionTest/CoreMotionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMotionTestApp.swift
+//  CoreMotionTest
+//
+//  Created by Vasya Smetankin on 15.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoreMotionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
